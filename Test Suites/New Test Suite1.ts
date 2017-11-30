53c818aad30e14c097406f62f20505190b7b2520
newjo <newjo@10.0.0.144>
2017-11-30 15:03:13 -0800

    Updated TestSuite

---
+++ Test Suites/New Test Suite1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2017-11-30T14:56:39</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7a79c49d-20c4-4a33-b396-1f9ee105a383</testSuiteGuid>
+</TestSuiteEntity>
