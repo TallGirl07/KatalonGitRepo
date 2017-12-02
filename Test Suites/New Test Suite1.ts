f708d23faa4e0bd19c118a38b7650edbbc6a48dd
unknown <newjourney49@gmail.com>
2017-12-01 17:24:20 -0700

    Testing Katalon Updates

--- Test Suites/New Test Suite1.ts
+++ Test Suites/New Test Suite1.ts
@@ -4,11 +4,23 @@
    <name>New Test Suite1</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2017-11-30T14:56:39</lastRun>
+   <lastRun>2017-12-01T17:08:02</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>7a79c49d-20c4-4a33-b396-1f9ee105a383</testSuiteGuid>
+   <testCaseLink>
+      <guid>85f20e8e-9a21-4559-9894-cf3263bf52ea</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 1 by Tom</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>495b49e0-a297-4c40-b7b6-9c7db3b9f607</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 2 by Kim</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
